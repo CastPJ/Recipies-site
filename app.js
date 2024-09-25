db041573915533054c6782c000fa39243d1bb89c
CastPJ <JPajak.dev@gmail.com>
2024-09-25 10:16:31 +0200

    Preparations for adding recipies site - delegation now working

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-function addRecipeCard(recipe) {
-  const main = document.querySelector(".thumbnails");
+const main = document.querySelector(".thumbnails");
 
-  const card = document.createElement("div");
-  card.className = "card card-self";
-  card.style.width = "180px";
-  card.addEventListener("click", bodyEvent);
+// function addRecipeCard(recipe) {
+//   const card = document.createElement("div");
+//   card.className = "card card-self";
+//   card.style.width = "180px";
 
-  const img = document.createElement("img");
-  img.className = "card-img-top";
-  img.src = recipe.img;
-  img.alt = "Card image";
+//   const img = document.createElement("img");
+//   img.className = "card-img-top";
+//   img.src = recipe.img;
+//   img.alt = "Card image";
+//   img.addEventListener("click", bodyEvent);
 
-  const cardBody = document.createElement("div");
-  cardBody.className = "card-body";
+//   const cardBody = document.createElement("div");
+//   cardBody.className = "card-body";
 
-  const cardTitle = document.createElement("h5");
-  cardTitle.className = "card-title";
-  cardTitle.textContent = recipe.name;
+//   const cardTitle = document.createElement("h5");
+//   cardTitle.className = "card-title";
+//   cardTitle.textContent = recipe.name;
 
-  const button = document.createElement("a");
-  button.href = "#";
-  button.className = "btn btn-secondary btn-self";
-  button.textContent = "Dodaj";
-  button.addEventListener("click", buttonEvent);
+//   const button = document.createElement("a");
+//   button.href = "#";
+//   button.className = "btn btn-secondary btn-self";
+//   button.textContent = "Dodaj";
+//   button.addEventListener("click", buttonEvent);
 
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(button);
-  card.appendChild(img);
-  card.appendChild(cardBody);
+//   cardBody.appendChild(cardTitle);
+//   cardBody.appendChild(button);
+//   card.appendChild(img);
+//   card.appendChild(cardBody);
 
-  main.appendChild(card);
+//   main.appendChild(card);
+// }
+
+function loadRecipe(meal) {
+  console.log(meal.name);
 }
 
 function fetchData() {
@@ -50,7 +54,24 @@ function buttonEvent(e) {
 
 function bodyEvent(e) {
   if (!e.target.classList.contains("btn")) {
-    console.log("PRZEPIS");
+    const mealID = e.target.src.substring(
+      e.target.src.lastIndexOf("/") + 1,
+      e.target.src.lastIndexOf(".")
+    );
+    fetch("data/bazadan.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const meal = data[mealID];
+        if (meal) {
+          main.innerHTML = "";
+          loadRecipe(meal);
+        } else {
+          console.error("Nie znaleziono dania o podanym ID:", mealId);
+        }
+      })
+      .catch((error) =>
+        console.error("Błąd podczas pobierania danych:", error)
+      );
   } else {
     return;
   }
